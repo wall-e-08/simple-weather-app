049f3f19ab63d46f96181ea6d852941c97eb1c4b
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2025-08-12 19:36:10 +0100

    add basic middlewares

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import bodyParser from "body-parser";
+import cors from 'cors';
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+
 import {OpenWeatherAPI} from "./openWeatherAPI";
 
 
@@ -9,6 +14,31 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT as string || 3001;
 const API_KEY = process.env.OPENWEATHER_API_KEY as string;
+
+// Enable CORS
+app.use(cors({
+  origin: ['http://localhost:3000', ], // todo: add frontend main url
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type',],
+  credentials: false
+}));
+
+const rateLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 5,
+  message: 'Too many requests from this IP, please try again later.'
+});
+
+app.use(rateLimiter);
+
+// Helmet for security headers
+app.use(helmet());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/', (req: Request, res: Response) => {
   console.log(API_KEY)
